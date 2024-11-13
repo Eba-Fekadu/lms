d3b33efd1b03b8c55bfac2741e58f0f807303b26
Eba Fekadu <ebafekadu@gmail.com>
2024-11-13 15:52:04 +0300

    Initial Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import {
+  ClerkProvider,
+  UserButton
+ 
+} from '@clerk/nextjs'
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,12 +30,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
+    
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="flex justify-between">
+        <UserButton showName />
+      </header>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
